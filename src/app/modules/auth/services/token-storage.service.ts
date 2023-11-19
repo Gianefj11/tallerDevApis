55b75f2285d7c532b990ff2950d049e72b807704
Gian Furlan <naigdeveloper@gmail.com>
2023-11-18 20:25:03 -0400

    signOut

--- src/app/modules/auth/services/token-storage.service.ts
+++ src/app/modules/auth/services/token-storage.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 const token = 'auth_session';
 @Injectable({
   providedIn: 'root'
 })
 export class TokenStorageService {
 
-  constructor() { }
+  constructor( private router: Router,) { }
 
 
   signOut(): void {
     window.sessionStorage.clear();
+    this.router.navigate(['/auth']);
   }
 
   public SaveToken(session: any): void {
